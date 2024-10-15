3d708ab691e7104c47551a4bce3d3c23c3cde2bc
JustGithubProject <Kropivadanil@gmail.com>
2024-10-15 23:40:25 +0300

    Defined createInstance method and filled some struct

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ public:
     }
 private:
     GLFWwindow* window;
+    VkInstance instance;
 
     void initWindow() {
         // Initialize GLFW library
@@ -34,7 +35,17 @@ private:
     }
 
     void initVulkan() {
+        createInstance();
+    }
 
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
     }
 
     void mainLoop() {
