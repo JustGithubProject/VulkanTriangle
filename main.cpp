286efdc2c3aa1e14b3b3e5cc96cf4bd00ff76f00
JustGithubProject <Kropivadanil@gmail.com>
2024-10-17 17:35:06 +0300

    Defined getRequiredExtensions method to get vector of extensions

--- main.cpp
+++ main.cpp
@@ -126,6 +126,19 @@ private:
         
         return true;
     }
+
+    std::vector<const char*> getRequiredExtensions() {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+        if (enableValidationLayers) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        return extensions;
+    }
 };
 
 int main() {
