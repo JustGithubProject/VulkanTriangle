1550c448e3899e6fa5783c35e7d8e63aa4c53316
JustGithubProject <Kropivadanil@gmail.com>
2024-10-20 20:59:31 +0300

    Defined pickPhysicalDevice method

--- main.cpp
+++ main.cpp
@@ -94,6 +94,27 @@ private:
             throw std::runtime_error("Failed to create window surface");
         }
     }
+
+    void pickPhysicalDevice() {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+        if (deviceCount == 0) {
+            throw std::runtime_error("Failed to find GPU's with Vulkan support");
+        }
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        for (const auto& device : devices) {
+            if (isDeviceSuitable(device)) {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE) {
+            throw std::runtime_error("Failed to find a suitable GPU")
+        }
+    }
 };
 
 int main() {
